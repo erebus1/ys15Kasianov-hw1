709c43e784474c9b55ac12e7fb8e8f9a3c6b5fa7
erebus1 <kasyanov_igor@inbox.ru>
2015-09-21 22:20:03 +0300

    Constructors, average, deviation realization & tests

--- src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
@@ -1,23 +1,68 @@
 package ua.yandex.shad.tempseries;
 
-public class TemperatureSeriesAnalysis {    
+import java.util.InputMismatchException;
 
+public class TemperatureSeriesAnalysis {
+    private double[] temperatureSeries;
+
+    /**
+     * initialize temperatureSeries as empty array
+     */
     public TemperatureSeriesAnalysis() {
-        
+        temperatureSeries = new double[0];
     }
-    
+
+    /**
+     * @throws  InputMismatchException if array contain temperature less than absolute zero (-273)
+     * @param temperatureSeries - contain temperatures not less than absolute zero (-273)
+     *
+     */
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
+        for (double temperature:temperatureSeries){
+            if (temperature < -273){
+                throw new InputMismatchException();
+            }
+        }
+        this.temperatureSeries = new double[temperatureSeries.length];
+        System.arraycopy(temperatureSeries, 0, this.temperatureSeries, 0, temperatureSeries.length);
         
     }
-    
+
+    /**
+     * @throws IllegalArgumentException if array is empty or non initialised
+     * @return average of temeratureSeries
+     */
     public double average(){        
-        return 0;
-    }    
-    
-    public double deviation(){
-        return 0;
+        if (temperatureSeries == null || temperatureSeries.length == 0){
+            throw new IllegalArgumentException();
+        }
+
+        double sum = 0;
+        for (double temperature:temperatureSeries){
+            sum += temperature;
+        }
+        return sum/temperatureSeries.length;
     }
-    
+
+    /**
+     * @throws IllegalArgumentException if array is empty or non initialised
+     * @return deviation of sample (divide on (n-1) not n)
+     */
+    public double deviation(){
+        if (temperatureSeries == null || temperatureSeries.length == 0){
+            throw new IllegalArgumentException();
+        }
+
+        double average = this.average();
+        double sum = 0;
+        for (double temperature:temperatureSeries){
+            sum += (temperature - average)*(temperature - average);
+        }
+
+        return Math.sqrt(sum/(temperatureSeries.length - 1));
+    }
+
+
     public double min(){
         return 0;
     }
--- src/test/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysisTest.java
@@ -1,19 +1,77 @@
 package ua.yandex.shad.tempseries;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.InputMismatchException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
 public class TemperatureSeriesAnalysisTest {
-    
+
+
+    @Test
+    public void testConstructorReturnObjectOnAppropriateArray(){
+        double[] arg = {1, 2, -273.0, 0, 300};
+        assertNotNull(new TemperatureSeriesAnalysis(arg));
+
+    }
+    @Test
+    public void testConstructorWithoutArgsReturnObject(){
+        assertNotNull(new TemperatureSeriesAnalysis());
+    }
+
+
+    @Test(expected = InputMismatchException.class)
+    public void testConstructorThrowExceptionWhenTemperatureLessThanAbsoluteZero(){
+        double[] arg = {1, 2, 23, -273.1};
+        new TemperatureSeriesAnalysis(arg);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAverageOfObjectConstructedWithoutArgsThrowsException(){
+        TemperatureSeriesAnalysis temperatureSeriesAnalysis = new TemperatureSeriesAnalysis();
+        temperatureSeriesAnalysis.average();
+
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testAverageOfObjectConstructedOnEmptyArrayThrowsException(){
+        TemperatureSeriesAnalysis temperatureSeriesAnalysis = new TemperatureSeriesAnalysis(new double[0]);
+        temperatureSeriesAnalysis.average();
+
+    }
+
     @Test
     public void testAverage() {
-        double[] temperatureSeries = {1.0, -5.0, 1.0, 5.0};
+        double[] temperatureSeries = {1.0, -5.0, 1.0, 6.0};
         TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
-        double expResult = 0.0;
+        double expResult = 0.75;
         double actualResult = seriesAnalysis.average();
-        
+
         assertEquals(expResult, actualResult, 0.00001);
-        
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeviationThrowsExceptionOnEmptyArray(){
+        TemperatureSeriesAnalysis temperatureSeriesAnalysis = new TemperatureSeriesAnalysis(new double[0]);
+        temperatureSeriesAnalysis.deviation();
+    }
+
+    @Test
+    public void testDeviation(){
+        double[] arg = {1, 3, -5, 23, 444};
+        TemperatureSeriesAnalysis temperatureSeriesAnalysis = new TemperatureSeriesAnalysis(arg);
+        double expextedResult = 196.385335;
+        double actualResult = temperatureSeriesAnalysis.deviation();
+        assertEquals(expextedResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testFindTempClosestToZeroOnOneElementArray() {
+
     }
     
 }
